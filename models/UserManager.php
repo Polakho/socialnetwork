90359479a8bf3be77506d228174d77304568c363
Polakho <armatys.alex@gmail.com>
2022-05-10 09:36:35 +0200

    Simple registration process

--- models/UserManager.php
+++ models/UserManager.php
@@ -33,4 +33,28 @@ function GetUserIdFromUserAndPassword($user, $pwd){
         var_dump($user);
         return $user["id"];
     }
+
+    function IsNicknameFree($nickname)
+    {
+        global $PDO;
+        $response = $PDO->prepare("SELECT * FROM user WHERE nickname = :nickname ");
+        $response->execute(
+            array(
+                "nickname" => $nickname
+            )
+        );
+        return $response->rowCount() == 0;
+    }
+
+    function CreateNewUser($nickname, $password)
+    {
+        global $PDO;
+        $response = $PDO->prepare("INSERT INTO user (nickname, password) values (:nickname , :password )");
+        $response->execute(
+            array(
+                "nickname" => $nickname,
+                "password" => $password
+            )
+        );
+        return $PDO->lastInsertId();
 }
